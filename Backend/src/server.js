9ad5300bdc9d292c39a82a99949f85130a59ff2a
Aman Raj <araj2271770@gmail.com>
2026-03-24 21:48:29 +0530

    change in url and cores

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -11,8 +11,8 @@ const app = express();
 
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "*",
-  credentials: true
+    origin: ["https://student-portal-frontend-s881.onrender.com", "http://localhost:5173"], 
+    credentials: true
 }));
 
 app.use(cookieParser());
--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
 const API = axios.create({
-    baseURL: "http://localhost:5001/api",
-    withCredentials: true,
+    baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5001/api',
+    withCredentials: true
 });
 
 export default API;
